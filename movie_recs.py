3fabafa9eb753b4eb0071df6e97911c58e69e043
AyushSingh916 <ayushsingh916924@gmail.com>
2024-03-09 10:06:25 +0530

    initial commit

---
+++ movie_recs.py
@@ -0,0 +1,30 @@
+import pymongo
+import requests
+
+client = pymongo.MongoClient("mongodb+srv://ayush_singh_916:inVjz9qFeXLogq67@cluster0.vgfmmoj.mongodb.net/")
+db = client.sample_mflix
+collection = db.movies
+
+API_URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+headers = {"Authorization": "Bearer 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"}
+
+def generate_embedding(text: str) -> list[float]:
+    response = requests.post(API_URL, headers=headers, json={"inputs":text})
+    if response.status_code != 200:
+        raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+    return response.json()
+
+query = "imaginary characters from outer space at war"
+
+results = collection.aggregate([
+    {"$vectorSearch": {
+        "queryVector": generate_embedding(query),
+        "path": "plot_embedding_hf",
+        "numCandidates": 100,
+        "limit": 4,
+        "index": "PlotSemanticSearch"
+    }}
+])
+
+for document in results:
+    print(f"Movie Name: {document['title']},\nMovie Plot: {document['plot']}\n")
